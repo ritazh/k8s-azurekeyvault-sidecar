0f59398f0eb6ed0f32f78c4c48ae0cf13593c3ba
Rita Zhang <rita.z.zhang@gmail.com>
2018-05-28 23:47:41 -0700

    Periodically pull secret

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 	"path"
 	"syscall"
 	"github.com/golang/glog"
+	"time"
 	
 	kvmgmt "github.com/Azure/azure-sdk-for-go/services/keyvault/mgmt/2016-10-01/keyvault"
 	kv "github.com/Azure/azure-sdk-for-go/services/keyvault/2016-10-01/keyvault"
@@ -24,7 +25,6 @@ import (
 const (
 	program					= "k8s-azurekeyvault-sidecar"
 	version     			= "0.0.1"
-	configFilePath  		= "azure.json"
 	permission  os.FileMode = 0400
 )
 
@@ -34,8 +34,12 @@ type Option struct {
 	vaultName string
 	// the name of the Azure Key Vault secret
 	secretName string
+	// the resourcegroup of the Azure Key Vault
+	resourceGroup string
 	// directory to save data
 	dir string
+	// file path to retrieve auth data
+	configFilePath string
 	// version flag
 	showVersion bool
 	// azure configs 
@@ -51,7 +55,6 @@ func main() {
 	sigChan := make(chan os.Signal, 1)
 	// register for SIGTERM (docker)
 	signal.Notify(sigChan, syscall.SIGTERM)
-
 	if err := parseConfigs(); err != nil {
 		showUsage("invalid config, %s", err)
 	}
@@ -59,21 +62,13 @@ func main() {
 		fmt.Printf("%s %s\n", program, version)
 		fmt.Printf("%s \n", options.azConfig.SubscriptionID)
 	}
+	_, err := os.Lstat(options.dir)
+	if err != nil {
+		showError("failed to get directory %s, error: %s", options.dir, err)
+	}
+
 	glog.Infof("starting the %s, %s", program, version)
-
 	kvClient := kv.New()
-
-	vaultUrl, err := getVault(ctx, options.azConfig.SubscriptionID, options.vaultName)
-	if err != nil {
-		showError("failed to get key vault, error: %s", err)
-	}
-
-	token, err := GetKeyvaultToken(AuthGrantType(), configFilePath)
-	if err != nil {
-		showError("failed to get token, error: %s", err)
-	}
-	
-	kvClient.Authorizer = token
 
 	go func() {
 		for {
@@ -84,33 +79,55 @@ func main() {
 			} 
 		}
 	}()
-	
-	/// TODO: make this a loop later
-	secret, err := kvClient.GetSecret(ctx, *vaultUrl, options.secretName, "")
-	if err != nil {
-		showError("failed to get secret, error: %s", err)
-	}
-	glog.Infof("secret: %s", *secret.Value)
 
-	_, err = os.Lstat(options.dir)
-	if err != nil {
-		showError("failed to get directory %s, error: %s", options.dir, err)
-	}
+	ticker := time.NewTicker(30 * time.Second)
+	quit := make(chan struct{})
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				fileInfo, err := os.Lstat(path.Join(options.dir, options.secretName))
+				if fileInfo != nil && err == nil {
+					glog.V(0).Infof("secret %s already exists in %s", options.secretName,options.dir)
+				} else {
+					vaultUrl, err := getVault(ctx, options.azConfig.SubscriptionID, options.vaultName, options.resourceGroup, options.configFilePath)
+					if err != nil {
+						showError("failed to get key vault, error: %s", err)
+					}
 
-	fileInfo, err := os.Lstat(path.Join(options.dir, options.secretName))
-	if fileInfo != nil {
-		glog.V(0).Infof("secret %s already exists in %s", options.secretName,options.dir)
-	} else {
-		if err = ioutil.WriteFile(path.Join(options.dir, options.secretName), []byte(*secret.Value), permission); err != nil {
-			showError("azure KeyVault failed to write secret %s at %s with err %s", options.secretName, options.dir, err)
+					token, err := GetKeyvaultToken(AuthGrantType(), options.configFilePath)
+					if err != nil {
+						showError("failed to get token, error: %s", err)
+					}
+					
+					kvClient.Authorizer = token
+
+					secret, err := kvClient.GetSecret(ctx, *vaultUrl, options.secretName, "")
+					if err != nil {
+						showError("failed to get secret, error: %s", err)
+					}
+					glog.Infof("secret: %s", *secret.Value)
+					if err = ioutil.WriteFile(path.Join(options.dir, options.secretName), []byte(*secret.Value), permission); err != nil {
+						showError("azure KeyVault failed to write secret %s at %s with err %s", options.secretName, options.dir, err)
+					}
+					glog.V(0).Infof("azure KeyVault wrote secret %s at %s", options.secretName,options.dir)
+				}
+			case <-quit:
+				ticker.Stop()
+				return
+			}
 		}
-		glog.V(0).Infof("azure KeyVault wrote secret %s at %s", options.secretName,options.dir)
-	}
+	}()
+
+	// run forever until signal
+	select {}		
 }
 
 func parseConfigs() error {
 	options.vaultName = *flag.String("vaultName", getEnv("VAULT_NAME", ""), "Name of Azure Key Vault instance.")
 	options.secretName = *flag.String("secretName", getEnv("SECRET_NAME", ""), "Name of Azure Key Vault secret.")
+	options.resourceGroup = *flag.String("resourceGroup", getEnv("RESOURCE_GROUP", ""), "Resource group name of Azure Key Vault.")
+	options.configFilePath = *flag.String("configFilePath", getEnv("CONFIG_FILEPATH", ""), "File path to retrieve auth data")
 	options.dir = *flag.String("dir", getEnv("DIR", ""), "Directory path to write data.")
 	options.showVersion = *flag.Bool("version", true, "Show version.")
 
@@ -122,11 +139,17 @@ func parseConfigs() error {
 	if options.secretName == "" {
 		return fmt.Errorf("env SECRET_NAME is unset")
 	}
+	if options.resourceGroup == "" {
+		return fmt.Errorf("env RESOURCE_GROUP is unset")
+	}
+	if options.dir == "" {
+		return fmt.Errorf("env CONFIG_FILEPATH is unset")
+	}
 	if options.dir == "" {
 		return fmt.Errorf("env DIR is unset")
 	}
 
-	options.azConfig, _ = GetAzureAuthConfig(configFilePath)
+	options.azConfig, _ = GetAzureAuthConfig(options.configFilePath)
 	if options.azConfig.SubscriptionID == "" {
 		return fmt.Errorf("Missing SubscriptionID in azure config")
 	}
@@ -160,12 +183,19 @@ func showError(message string, args ...interface{}) {
 	os.Exit(0)
 }
 
-func getVault(ctx context.Context, subscriptionID string, vaultName string) (vaultUrl *string, err error) {
+func getVault(ctx context.Context, subscriptionID string, vaultName string, resourceGroup string, configFilePath string) (vaultUrl *string, err error) {
+	glog.Infof("subscriptionID: %s", subscriptionID)
+	glog.Infof("vaultName: %s", vaultName)
+	glog.Infof("configFilePath: %s", configFilePath)
+	glog.Infof("resourceGroup: %s", resourceGroup)
+
 	vaultsClient := kvmgmt.NewVaultsClient(subscriptionID)
 	token, _ := GetManagementToken(AuthGrantType(), configFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get management token, error: %v", err)
+	}
 	vaultsClient.Authorizer = token
-	resourceGroup, err := GetResourceGroup(configFilePath)
-	vault, err := vaultsClient.Get(ctx, *resourceGroup, vaultName)
+	vault, err := vaultsClient.Get(ctx, resourceGroup, vaultName)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get vault, error: %v", err)
 	}
